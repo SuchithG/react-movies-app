d90a44a04189bd1dec402b50d1a87e1031dd9ef9
SuchithG <suchithgowda1995@gmail.com>
2023-03-30 22:36:46 +0530

    feat: :zap: sending a GET request to swapi to fetch list of movies

--- src/App.js
+++ src/App.js
@@ -5,20 +5,29 @@ import "./App.css";
 
 function App() {
   const [movies, setMovies] = useState([]);
+
   function fetchMoviesHandler() {
-    fetch("https://swapi.dev/films/")
+    fetch("https://swapi.dev/api/films/")
       .then((response) => {
-        return response.json;
+        return response.json();
       })
       .then((data) => {
-        setMovies(data.results);
+        const transformedMovies = data.results.map((movieData) => {
+          return {
+            id: movieData.episode_id,
+            title: movieData.title,
+            openingText: movieData.opening_crawl,
+            releaseDate: movieData.release_date,
+          };
+        });
+        setMovies(transformedMovies);
       });
   }
 
   return (
     <React.Fragment>
       <section>
-        <button>Fetch Movies</button>
+        <button onClick={fetchMoviesHandler}>Fetch Movies</button>
       </section>
       <section>
         <MoviesList movies={movies} />
